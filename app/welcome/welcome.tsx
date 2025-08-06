b49196e5829b0aa279f848a0adc2abcaaea6c4c8
Garfield Gordon <garfield.gordon@btinternet.com>
2025-08-06 17:24:20 +0100

    updated again

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -22,7 +22,8 @@ export function Welcome() {
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next? Make a change again and it will be deployed to 
+              What&apos;s next? Make a change again and it will be deployed to
+              the staging app automatically.  
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
