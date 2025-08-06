a5dc3d7739dc0793d207c7754ba30bbccaa46b56
Garfield Gordon <garfield.gordon@btinternet.com>
2025-08-06 19:34:24 +0100

    updated github action

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <Welcome />
+      <div className="flex justify-center mt-8">
+        <Link
+          to="/users-simple"
+          className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors font-medium"
+        >
+          🗄️ View Users Database Demo
+        </Link>
+      </div>
+    </div>
+  );
 }
